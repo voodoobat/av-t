0288009239805b8b6ef879b35490a9afe3af9d33
Vova Rodin <mynameisvova@yahoo.com>
2023-04-12 19:17:51 +0300

    LayoutWideSection component

---
+++ components/layout/LayoutWideSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wide-section">
+    <div class="wide-section__header" :class="headerModifiers">
+      <div v-if="heading" class="wide-section__heading">
+        {{ heading }}
+      </div>
+      <slot name="header" />
+    </div>
+    <div class="wide-section__body">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  heading: {
+    type: String,
+    default: () => ''
+  },
+  headerModifiers: {
+    type: Array,
+    default: () => []
+  }
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
     <PageContainer heading="Настройки">
-      <h1>...</h1>
+      <div class="settings-section">
+        <div class="settings-section">
+          <LayoutWideSection
+            class="settings-section__subsection"
+            heading="Звонок через SIP"
+            :header-modifiers="['is-flex']"
+          >
+            <div class="post-entry">
+              <p class="is-muted">
+                Включите эту функцию чтобы Авито и другие площадки не блокировали ваш аккаунт. Будет выглядеть так, будто звонки совершаются с разных номеров.
+              </p>
+            </div>
+          </LayoutWideSection>
+        </div>
+      </div>
     </PageContainer>
   </div>
 </template>
-
-<script setup>
-</script>
